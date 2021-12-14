b96d21fb1a8ec5c655708e404aef0c056b8bfdc1
Austin <69608231+walley1@users.noreply.github.com>
2021-12-13 20:00:52 -0500

    Update PhoneBookEntry.java

--- PhoneBookEntry.java
+++ PhoneBookEntry.java
@@ -1,6 +1,5 @@
-//Add class comment and @ tags
  /**
-  * This class represents a phone book entry (contact) that has a first name, a last name, and a phone number.
+  * This class represents a phone book entry that has a first name, a last name, and a phone number.
   * 
   * @author Austin Walley
   * @version 10/23/2021
@@ -11,13 +10,7 @@ public class PhoneBookEntry implements Comparable<PhoneBookEntry>
     private String fName;
     private String lName;
     private String pNum;
-
-    /** 
-     * Constructor that takes the two names and phone number as param.
-     * @param fName if valid.
-     * @param lName if valid.
-     * @param pNum if valid.
-     * */         
+    
     public PhoneBookEntry(String fName, String lName, String pNum) 
     {
         if (fName == null)
@@ -34,10 +27,6 @@ public class PhoneBookEntry implements Comparable<PhoneBookEntry>
         }
     }
 
-    /** 
-     * Mutator method that sets first name.
-     * @param fName if valid.
-     * */
     public void setFirstName(String fName) 
     {
         if (fName == null)
@@ -47,10 +36,6 @@ public class PhoneBookEntry implements Comparable<PhoneBookEntry>
         this.fName = fName;
     }
     
-    /** 
-     * Mutator method that sets last name.
-     * @param lName if valid.
-     * */
     public void setLastName(String lName) 
     {
         if (lName == null)
@@ -60,10 +45,6 @@ public class PhoneBookEntry implements Comparable<PhoneBookEntry>
         this.lName = lName;
     }
     
-    /** 
-     * Mutator method that sets phone number.
-     * @param pNum if valid.
-     * */
     public void setPhoneNum(String pNum) 
     {
         if (pNum == null)
@@ -73,47 +54,26 @@ public class PhoneBookEntry implements Comparable<PhoneBookEntry>
         this.pNum = pNum;
     }
     
-    /** 
-     * Accessor method that gets first name.
-     * @return first name.
-     * */
     public String getFirstName() 
     {
         return fName;
     }
     
-    /** 
-     * Accessor method that gets last name.
-     * @return last name.
-     * */
     public String getLastName() 
     {
         return lName;
     }
     
-    /** 
-     * Accessor method that gets phone number.
-     * @return phone number.
-     * */
     public String getPhoneNum() 
     {
         return pNum;
     }
     
-    /** 
-     * toString method that returns phone book entry.
-     * @return formatted phone book entry.
-     * */
     public String toString() 
     {
         return "Name: " + fName + " " + lName + "\nPhone Number: " + pNum;
     }
-    
-    /** 
-     * Compareto method that sorts phone book entries.
-     * @param e PhoneBookEntry.
-     * @return e PhoneBookEntry at index.
-     * */
+
     public int compareTo(PhoneBookEntry e) 
     {
         if (this.lName == e.lName)
