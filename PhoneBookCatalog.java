4ba946c58f010adb9e32ba49994f13a319bd8ccf
Austin <69608231+walley1@users.noreply.github.com>
2021-12-13 19:58:46 -0500

    Update PhoneBookCatalog.java

--- PhoneBookCatalog.java
+++ PhoneBookCatalog.java
@@ -1,7 +1,6 @@
 import java.util.*;
-//Add class comment and @ tags
  /**
-  * This class will model a phone book catalog that contains individual PhoneBookEntry objects.
+  * This class models a phone book catalog that contains individual PhoneBookEntry objects.
   * 
   * @author Austin Walley
   * @version 10/23/2021
@@ -11,18 +10,11 @@ public class PhoneBookCatalog
 {
     private ArrayList<PhoneBookEntry> phonebook;
     
-    /** 
-     * Constructor that takes no param and initializes instance variable as ArrayList of type PhoneBookEntry.
-     * */
     public PhoneBookCatalog() 
     {
         phonebook = new ArrayList<>();
     }
     
-    /** 
-     * The addEntry method that takes a PhoneBookEntry object as a param and adds it to the ArrayList. 
-     * @param pbEntry object.
-     * */
     public void addEntry(PhoneBookEntry pbEntry) 
     {
         if (pbEntry == null) 
@@ -31,12 +23,7 @@ public class PhoneBookCatalog
         }
         phonebook.add(pbEntry);
     }
-    
-    /** 
-     * The getEntry method that takes an int as param and returns the entry located at that index in the ArrayList.
-     * @param index int.
-     * @return PhoneBookEntry at index.
-     * */
+
     public PhoneBookEntry getEntry(int index) 
     {
         if (index < 0 || index >= phonebook.size())
@@ -46,11 +33,6 @@ public class PhoneBookCatalog
         return phonebook.get(index);
     }
     
-    /** 
-     * The removeEntry method that takes an int as param and removes the entry located at that index in the ArrayList
-     * @param index int
-     * @return PhoneBookEntry at index 
-     * */
     public PhoneBookEntry removeEntry(int index) 
     {
         if (index < 0 || index >= phonebook.size())
@@ -59,19 +41,12 @@ public class PhoneBookCatalog
         }
         return phonebook.remove(index);
     }
-
-    /** 
-     * The sort() method that uses the Collections sort method to sort the catalog.
-     * */    
+   
     public void sort()
     {
         Collections.sort(phonebook);
     }
     
-    /** 
-     * The toString() method that formats the phone book.
-     * @return output of the formatted string.
-     * */
     public String toString() 
     {
         String output = "";
